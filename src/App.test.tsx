78ad75c2e454e231f9626474c3b8fd452726e3cc
Grimm <rafalmirkowski@gmail.com>
2023-06-04 16:01:18 +0200

    add eslint & prettier boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
-import {render, screen} from "@testing-library/react";
-import App from './App'
-import {describe, it} from "vitest";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { describe, it, expect } from 'vitest';
 
 //TODO: remove this test  after forking repo
 describe('App basic test', () => {
-    it('Should render greetings', () => {
-        const {debug} = render(<App/>)
-       debug()
-    })
-})
+  it('Should render greetings', () => {
+    render(<App />);
+    expect(screen.getByText('hello there')).toBeDefined();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 import './index.css';
 
-const App = () => {
-    return (<> Happy Coding! </>)
-};
+const App = (): React.ReactElement => (
+  <div>
+    <span>hello there</span>
+  </div>
+);
 
 export default App;
